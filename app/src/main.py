2281f54d7cc88d3954fffc9b4ee9a41468c574b9
taxintt <nthouse14@gmail.com>
2022-12-04 12:11:08 +0900

    feat(sample.py): set span for /sample endpoint

--- app/src/main.py
+++ app/src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 
+from .config.middleware import Middlewares
 from .config.opentelemetry import setup_fastapi_instrumentor, setup_span_exporter
 from .router.sample import sample_router
 
@@ -8,3 +9,5 @@ app.include_router(sample_router)
 
 setup_fastapi_instrumentor(app)
 setup_span_exporter()
+
+Middlewares(app)
